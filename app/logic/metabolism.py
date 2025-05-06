4a034122efc638d6410b8374cdc833f74a14eea0
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-05-06 16:35:40 -0300

    feat - def calcular tdee

--- app/logic/metabolism.py
+++ app/logic/metabolism.py
@@ -3,3 +3,12 @@ def calcular_tbm(peso,altura,idade,sexo):
         return 10 * peso + 6.25 * altura - 5 * idade + 5
     else:
         return 10 * peso + 6.25 * altura - 5 * idade - 161
+    
+def calcular_tdee(tbm, nivel_atividade):
+    fatores = {
+        'sedentario': 1.2,
+        'leve': 1.375,
+        'moderado': 1.55,
+        'intenso': 1.725,
+        'muito_intenso': 1.9
+    }
