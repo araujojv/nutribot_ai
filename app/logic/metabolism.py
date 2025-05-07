1b00c2660726a98e6d53f5daf40f0a3d51beb986
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-05-07 09:55:36 -0300

    fix - def calcular_tmb

--- app/logic/metabolism.py
+++ app/logic/metabolism.py
@@ -1,4 +1,4 @@
-def calcular_tbm(peso,altura,idade,sexo):
+def calcular_tmb(peso,altura,idade,sexo):
     if sexo.lower() == 'masculino':
         return 10 * peso + 6.25 * altura - 5 * idade + 5
     else:
